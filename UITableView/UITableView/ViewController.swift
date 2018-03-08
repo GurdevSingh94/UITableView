1a047710b3e55837c1e670acf9b269c50406a8f9
GurdevSingh94 <gurdevrattan1@gmail.com>
2018-03-08 17:38:16 +0000

    Add files via upload

---
+++ UITableView/UITableView/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  UITableView
+//
+//  Created by Gurdev Singh on 07/03/2018.
+//  Copyright © 2018 Gurdev Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
+
